af8c5d567d75ae9b5263c855fed40abb417208ce
Toksi86 <puma-yellow@yandex.ru>
2024-03-02 13:28:49 +0500

    Реализована отправка сообщений о создании сбора, платежа и поступлении платежа через Selery

--- dcs/donation_app/tasks.py
+++ dcs/donation_app/tasks.py
@@ -1,13 +1,37 @@
 from __future__ import absolute_import, unicode_literals
+
 from celery import shared_task
 from django.core.mail import EmailMessage
 
+
 @shared_task
-def send_confirmation_email_task(user_email):
+def send_payment_confirmation_email_task(request):
     email = EmailMessage(
-        'Вы успешно создали пожертвование.',
-        'Успешное пожертвование',
+        'Вы отправили пожертвование',
+        f'Вы отправили пожертвование на сумму {request.data['amount']}',
         'dcs@example.com',
-        [user_email],
+        [request.user.email],
+    )
+    email.send()
+
+
+@shared_task
+def send_payment_received_email_task(request, collect_instance):
+    email = EmailMessage(
+        f'По вашему сбору {collect_instance.title} поступило пожертвование',
+        f'По вашему сбору {collect_instance.title} поступило пожертвование на сумму {request.data['amount']}.',
+        'dcs@example.com',
+        [collect_instance.author.email],
+    )
+    email.send()
+
+
+@shared_task
+def send_collect_confirmation_email_task(request):
+    email = EmailMessage(
+        'Успешное создание сбора',
+        f'Вы успешно создали сбор с заголовком {request.data['title']}.',
+        'dcs@example.com',
+        [request.user.email],
     )
     email.send()
